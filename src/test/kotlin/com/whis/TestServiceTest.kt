7bb58859340e4887c7a8f0e779df2095647ab8b6
whis <admin@wwhis.com>
2018-07-04 11:14:37 +0800

    [mod] 修改错误配置文件

--- src/test/kotlin/com/whis/TestServiceTest.kt
+++ src/test/kotlin/com/whis/TestServiceTest.kt
@@ -1,16 +1,12 @@
 package com.whis
 
-import com.documents4j.api.IConverter
 import com.whis.app.Application
-import com.whis.app.service.HttpTestService
-import com.whis.base.common.RequestUtil
+import com.whis.base.common.Util
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import java.io.File
 
 
 @SpringBootTest(classes = [(Application::class)])
@@ -20,11 +16,20 @@ class TestServiceTest {
     private val logger = LoggerFactory.getLogger(TestServiceTest::class.java)
 
 
-    @Autowired lateinit var httpTestService: HttpTestService
-
     @Test
     fun test() {
 
     }
 
+    @Test
+    fun utilSafeGet() {
+        val map = hashMapOf<String, Any>(
+                "name" to "whis",
+                "age" to 25
+        )
+
+        logger.info("name {}", Util.safeGet(map, "name", String::class.java, ""))
+        logger.info("age {}", Util.safeGet(map, "age", Long::class.java, 0))
+        logger.info("gender {}", Util.safeGet(map, "gender", Int::class.java, 0))
+    }
 }
